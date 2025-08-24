8a0d71f6174cb6b4cb14fb22e1f80f1815158446
Nandhagopal-17 <vishnunandhu2003@gmail.com>
2025-08-24 18:54:54 +0530

    final changes appeared

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Nandha");
+function nandha(){
+    let nandha = [1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']; 
+    return nandha;
+}
+console.log(nandha());
---
+++ try.js
@@ -0,0 +1,24 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+// function toCelcius (f){
+//     return (5/9)*(f-32);
+// }
+// let value = toCelcius(87);
+// console.log(Math.round(value)
+// let myfunction = (a,b,c) =>{
+    
+// let result = myfunction(2,3,4);
+// }
+// console.log(result);
+// let message =0;
+// function sum(name,age){
+//     message = `hello ${name} you are ${age} years old`;
+// }
+// sum("Nandha",21);
+// console.log(message);
+let sum =0;
+function add(x,y){
+    sum = x+y;
+}
+add(5,3);
+console.log(sum)
